c8a1fd0f5a6f04daf4257dbb9a9a6b841ebdb1bc
danielvtfs <danielvtfs@gmail.com>
2022-04-01 11:59:38 -0300

    fixup! fixup! Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Deshboard } from "./components/Deshboard/Deshboard";
 import { Header } from "./components/Header/header";
+import { NewTransactionModal } from "./components/NewTransactionModal/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [modalIsOpen, setModalIsOpenIsOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setModalIsOpenIsOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setModalIsOpenIsOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Deshboard />
+      <NewTransactionModal
+        isOpen={modalIsOpen}
+        onCloseNewTransactionModal={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
